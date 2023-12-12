8c58069ee629080f0fa3552280c01a552513cfc7
Amitk4600 <amitk4600@gmail.com>
2023-12-12 17:30:42 +0530

    task

---
+++ index.js
@@ -0,0 +1,18 @@
+const signBtn = document.getElementsByClassName('btn')
+
+function OnClickHandler (){
+   
+    //    return window.location.href="./index.html"
+   
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    let userDropdown = document.getElementById('userDropdown');
+
+    let userLoggedIn = true;
+
+    let liElement  = document.createElement('li');
+    liElement.classList.add('nav-item','dropdown','d-flex','fw-bold');
+})
+
+
